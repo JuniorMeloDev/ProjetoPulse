d0fad8162ab881073b6d9bce330c23458e7bef1d
Junior Melo <juninhotj_@hotmail.com>
2023-08-30 16:30:56 -0300

    Adicionandoo nextAuth com JWT

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,35 +2,57 @@ import NextAuth, { getServerSession } from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
-
   providers: [
     CredentialsProvider({
       name: "credentials",
       credentials: {
         email: { label: "email", type: "email" },
-        password: { label: "password", type: "password" },
+        senha: { label: "password", type: "password" },
       },
+      // credenciais cadastradas no banco (mesmos nomes que consta lá)
       async authorize(credentials, req) {
-        console.log(credentials);
-        const { email, password } = credentials;
-        const user = {
-          name: "Junior Melo",
-          email: "Junior@email.com",
-  
-          //const validacao = true
-          // if (validacao) {
-          //  throw new Error('Email e senha invalidos')
-        };
+        const res = await fetch('http://localhost:8080/auth/login', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ 
+            email: credentials?.email,
+            senha: credentials?.senha
+          }),
+          //faz a requisição no banco de dados onde tem os cadastros dos usuarios (POST)
+      });
+      const user = await res.json()
+
+      if (user) {
         return user;
+      } else {
+        return null;
+     }
+
       },
     }),
   ],
-  }
+  callbacks: {
+    async jwt({ token, user }) {
+      return { ...token, ...user };
+    },
+    async session({ session, token, user }) {
+      session.user = token;
+      return session;
+      //funções para usar o JWT token
+    },
+
+    pages: {
+      signIn: "/", 
+      error: "/", 
+    },
+  },
+  secret: process.env.SECRET,
+};
 
 export default NextAuth(authOptions);
 
 export const getServerAuthSession = (req, res) => {
-  return getServerSession(req, res, authOptions)
-}
-
- 
+  return getServerSession(req, res, authOptions);
+};
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,23 +1,28 @@
-import Link from "next/link";
-import { Inter } from "next/font/google";
-import { signIn, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import { useState } from "react";
 import styles from "../styles/login.module.css";
 
 
 export default function Home() {
-  const { data, status } = useSession();
+  const { session, status } = useSession();
 
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [senha, setSenha] = useState("");
   // serve para preecher o formulario do email e senha
 
-
-  function handleSignIn(e) {
+  async function handleSignIn(e) {
     e.preventDefault()
-    signIn('credentials', { email, password, callbackUrl: '/teste' })
+    signIn('credentials', { email, senha, callbackUrl: '/home' })
     // a função serve para validar o email e redireciona para a pagina logado
-  }
+
+    const response = await fetch('http://localhost:8080/listar/users', {
+      method: "Get",
+      headers: {
+        authorization: `bearer ${session?.user.accessToken}`,
+      },
+    });
+    // constante responsavel por pegar do banco de dados as informações dos usuarios ja cadastrados (GET). o 'bearer' é para acesso com o JWT
+  };
 
   return (
     <div>
@@ -25,7 +30,7 @@ export default function Home() {
         <form className={styles.formLogin}>
           <img className={styles.logo} src="imagens/logo.png" alt="logo" />
           <input
-            type="text"
+            type="email"
             placeholder="Digite seu Email"
             value={email}
             onChange={e => setEmail(e.target.value)}
@@ -33,17 +38,20 @@ export default function Home() {
           <input
             type="password"
             placeholder="Digite sua Senha"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
+            value={senha}
+            onChange={e => setSenha(e.target.value)}
           />
           <button type="submit" className={styles.btn}>
             LOGIN
           </button>
         </form>
         <h1>Status: {status}</h1>
+        <button onClick={signOut}>sair</button>
       </div>
+
     </div>
 
   )
 }
+
 
